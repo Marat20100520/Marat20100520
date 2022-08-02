f412a823c630baa7d036203de161d7375295849c
Marat20100520 <110460726+Marat20100520@users.noreply.github.com>
2022-08-02 12:52:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-Python bot Discord
+import discord
+from discord.ext import commands
+
+TOKEN = ("MTAwMjE4OimportDAyODM5MzA1MDE5Mg.GtvMVt.pNRVAitXeJ0sbV5_pe68gq36bOryocFtYuky_4")
+
+bot = commands.Bot(command_prefix=('!'))
+bot.remove_command( 'help' )
+
+@bot.event
+async def on_ready():
+    print("Я запущен!")
+
+@bot.command()
+async def hello(ctx):
+    embed = discord.Embed(
+        title="Привет всем!",    
+    )
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def say(ctx, *, arg):
+  await ctx.send(arg)
+
+bot.run(TOKEN)
